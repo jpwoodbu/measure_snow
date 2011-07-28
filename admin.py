b5d4f4693a42225d9b3855123b25a79a7b4f2036
Jonathan Woodbury <jpwoodbu@mybox.org>
2011-07-28 19:04:02 -0400

    Added some filters and search_fields to SnowfallMeasures for the admin

--- admin.py
+++ admin.py
@@ -3,6 +3,8 @@ from measure_snow.models import SnowSeason, SnowfallMeasure
 
 class SnowfallMeasureAdmin(admin.ModelAdmin):
     list_display = ('timestamp', 'season', 'inches')
+    list_filter = ['season', 'timestamp','inches']
+    search_fields = ['inches']
 
 admin.site.register(SnowSeason)
 admin.site.register(SnowfallMeasure, SnowfallMeasureAdmin)
